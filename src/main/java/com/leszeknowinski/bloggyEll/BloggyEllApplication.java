4c98d88986fed2d4cbda45445380fef4a7e1b2b2
LeszekNow <leszek.nowinski@icloud.com>
2018-06-27 22:02:30 +0200

    1st commit

---
+++ src/main/java/com/leszeknowinski/bloggyEll/BloggyEllApplication.java
@@ -0,0 +1,12 @@
+package com.leszeknowinski.bloggyEll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggyEllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggyEllApplication.class, args);
+	}
+}
